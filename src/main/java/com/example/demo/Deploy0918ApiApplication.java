f707a009d8aa645a24cab575a4dd2b6b7fcdf05a
hgb926 <hgb926@naver.com>
2024-09-18 20:53:20 +0900

    initial commit

---
+++ src/main/java/com/example/demo/Deploy0918ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy0918ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy0918ApiApplication.class, args);
+	}
+
+}
